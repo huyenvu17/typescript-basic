cd23de93d9321f516e3691f11a420dcd1d5af31f
Huyen Vu Ngoc <huyenvu.vnh@outlook.com>
2020-08-31 17:36:16 +0700

    prepare template

--- ThongTinNhanVien.ts
+++ ThongTinNhanVien.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+class NhanVien{
+    public maNhanVien:string;
+    public hoTen:string;
+    public loaiNhanVien:string;
+    public luongCB:number;
+    
+    constructor(HoTen:string, MaNhanVien:string, LoaiNhanVien:string, LuongCB:number){
+        this.hoTen = HoTen;
+        this.maNhanVien = MaNhanVien;
+        this.loaiNhanVien = LoaiNhanVien;
+        this.luongCB = LuongCB;
+    }
+    public xuatThongTinNhanVien(){
+
+    }
+    public themNhanVien(){
+        
+    }
+}
